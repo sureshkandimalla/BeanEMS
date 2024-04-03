137b2be570ee09bfc79dfac956b7ef68021a36e1
shivadeep <shivadeep123@gmail.com>
2024-04-02 20:56:06 -0500

    donuts added

--- src/WorkForce/WorkForce.jsx
+++ src/WorkForce/WorkForce.jsx
@@ -1,14 +1,19 @@
-import React,{useState} from "react";
+import React,{useState,useEffect,useRef} from "react";
 import { Tabs,Card, Row, Col, Button, Flex, Drawer, Menu } from 'antd';
-import {PlusOutlined} from '@ant-design/icons';
+import {PlusOutlined, RiseOutlined} from '@ant-design/icons';
 import Newemployee from "../Newemployee/Newemployee";
 import Active from "./Active";
 import WorkForceList from "./WorkForceList";
-
+import PieCharts from '../PieCharts/PieCharts';
 
 const WorkForce=()=>{
 
   const { TabPane } = Tabs;
+  const [workForceChartData, setWorkForceChartData] = useState([]);
+  const [workForceChartLabels, setWorkForceChartLabels] = useState([]);
+  const [invoicesChartData, setInvoicesChartData] = useState([]);
+  const [invoicesChartLabels, setInvoicesChartLabels] = useState([]);
+  const isInitialRender = useRef(true);
 
   const toggleTabs = (e) => {
   }
@@ -39,9 +44,42 @@ const WorkForce=()=>{
       console.log('click ', e);
       setCurrent(e.key);
     };
-    return(
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (!isInitialRender.current) {
+                try {
+                    const response1 = await fetch('http://localhost:8080/api/v1/employees/employeesCountByStatus');
+                    const data1 = await response1.json();
+
+                    // Assuming the response from your API is an array of objects with 'label' and 'value' properties
+                    const labels = data1.map(item => item.status);
+                    const chartData = data1.map(item => item.count);
+                    setWorkForceChartLabels(labels);
+                    setWorkForceChartData(chartData);
+
+                    const response2 = await fetch('http://localhost:8080/api/v1/invoice/invoicesCountByStatus');
+                    const data2 = await response2.json();
+                    const labels2 = data2.map(item => item.status);
+                    const chartData2 = data2.map(item => item.count);
+                    setInvoicesChartLabels(labels2);
+                    setInvoicesChartData(chartData2);
+
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                }
+            } else {
+                isInitialRender.current = false;
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const totalParamCount = workForceChartData[0] + workForceChartData[1] + workForceChartData[2] + workForceChartData[3];
+
+    return (
         <>
-        
             <Drawer
                 title={`Employee Onboarding`}
                 placement="right"
@@ -50,17 +88,49 @@ const WorkForce=()=>{
                 open={open}
             >
                 <Newemployee />
-
             </Drawer>
-           <Card>
-          
-           
-            <Tabs className='bean-home-tabs' defaultActiveKey="1" onChange={toggleTabs} items={items}
-                tabBarExtraContent={<Button type='primary' onClick={addNewEmployee}><PlusOutlined /> Add New Employee</Button>}>
-            </Tabs>
-       
-            
-            </Card> 
+            <Row gutter={19}>
+                <Col span={17}>
+                    <Row>
+                        <Col span={10}>
+                            <Card className='invoiceStatusCard'>
+                                <span className='invoiceCardTitle'>Invoice Status</span>
+                                <PieCharts chartData={invoicesChartData} chartLabels={invoicesChartLabels} />
+                            </Card>
+                        </Col>
+                        <Col span={14}>
+                            <Card className='totalworkForceCard'>
+                                <Row>
+                                    <Col span={18}><PieCharts chartData={workForceChartData} chartLabels={workForceChartLabels} /></Col>
+                                    <Col span={3}>
+                                        <div className="totalWorkFrcDiv">
+                                            <span className='totalWorkTitle' style={{ marginTop: "10px" }}>Total Work Force</span>
+                                            <Row justify="space-between">
+                                                <span className='totalWorkForceCount'>{totalParamCount}</span>
+                                                <span className='mrgTop15'><RiseOutlined className='riseIcon' /> <span> 3.5%</span></span>
+                                            </Row>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </Card>
+                        </Col>
+                        
+                        <Col span={10}>
+                            <Card className='invoiceStatusCard'>
+                                <span className='invoiceCardTitle'>Invoice Status</span>
+                                <PieCharts chartData={invoicesChartData} chartLabels={invoicesChartLabels} />
+                            </Card>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+
+
+            <Card>
+                <Tabs className='bean-home-tabs' defaultActiveKey="1" onChange={toggleTabs} items={items}
+                    tabBarExtraContent={<Button type='primary' onClick={addNewEmployee}><PlusOutlined /> Add New Employee</Button>}>
+                </Tabs>
+            </Card>
         </>
     );
 }
